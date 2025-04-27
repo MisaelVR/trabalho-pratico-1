b2cf7e7346f5a91ad44a98f78bb890910a451246
MisaelVR <108824334+MisaelVR@users.noreply.github.com>
2025-04-27 02:35:23 -0300

    Código Arrumado

--- Main.java
+++ Main.java
@@ -4,21 +4,17 @@ public class Main {
         Fila filaAtendimento = new Fila();
 
         try {
-            // Histórico - Pilha
             historico.empilhar(new Elemento(1, 101, 20240820.1030f));
             historico.empilhar(new Elemento(2, 102, 20240820.1100f));
 
-            // Atendimento - Fila
             filaAtendimento.enfileirar(new Elemento(1, 201, 0f));
             filaAtendimento.enfileirar(new Elemento(2, 202, 0f));
-
-            // Exibindo Histórico
+            
             System.out.println("Histórico de Solicitações:");
             while (!historico.estaVazia()) {
                 historico.desempilhar().imprimir();
             }
 
-            // Exibindo Atendimento
             System.out.println("\nFila de Atendimento:");
             while (!filaAtendimento.estaVazia()) {
                 filaAtendimento.desenfileirar().imprimir();
